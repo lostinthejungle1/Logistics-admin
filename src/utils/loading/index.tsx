fe194221d4afd15ad6f9b396912868fa0f840be9
Liang <lianginchina1999@gmail.com>
2024-07-09 15:58:33 +0800

    setup request loading version 2

--- src/utils/loading/index.tsx
+++ src/utils/loading/index.tsx
@@ -1,15 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Loading from "./Loading";
+import "./loading.less";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import Loading from "./Loading";
 
+// let count = 0;
+
+// export const showLoading = () => {
+// 	if (count === 0) {
+// 		const loading = document.createElement("div");
+// 		loading.id = "loading";
+// 		document.body.appendChild(loading);
+// 		ReactDOM.createRoot(loading!).render(<Loading tip="加载中" />);
+// 	}
+// 	count++;
+// };
+
+// export const closeLoading = () => {
+// 	count--;
+// 	if (count === 0) {
+// 		const loading = document.getElementById("loading");
+// 		if (loading) {
+// 			document.body.removeChild(loading);
+// 		}
+// 	}
+// };
 let count = 0;
 
 export const showLoading = () => {
 	if (count === 0) {
-		const loading = document.createElement("div");
-		loading.id = "loading";
-		document.body.appendChild(loading);
-		ReactDOM.createRoot(loading!).render(<Loading tip="加载中" />);
+		const loading = document.getElementById("loading");
+		if (loading) {
+			loading.style.display = "flex";
+		}
 	}
 	count++;
 };
@@ -19,7 +41,7 @@ export const closeLoading = () => {
 	if (count === 0) {
 		const loading = document.getElementById("loading");
 		if (loading) {
-			document.body.removeChild(loading);
+			loading.style.display = "none";
 		}
 	}
 };
