503b70d86f456ba5a41729b31dc851415342e6b9
Liang <lianginchina1999@gmail.com>
2024-07-21 16:22:41 +0800

    update request util function

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
-import { message } from "antd";
+// import { message } from "antd";
 import { closeLoading, showLoading } from "./loading";
 import { Result } from "@/types/api";
 import storage from "./storage";
+import { message } from "./AntdGlobal";
 
 // Create an axios instance
 const instance = axios.create({
@@ -15,7 +16,10 @@ const instance = axios.create({
 //create request interceptor - add auth token
 instance.interceptors.request.use(
 	(config) => {
-		showLoading();
+		if (config.showLoading) {
+			showLoading();
+		}
+		// showLoading();
 		const token = storage.get("token");
 		config.headers.icode = "23B3904E1E8DE5DC";
 		if (token) {
@@ -34,17 +38,25 @@ instance.interceptors.response.use(
 	(response) => {
 		// console.log(response);
 		const data: Result = response.data;
-		closeLoading();
-		if (data.code === 50001) {
+		if (response.config.showLoading) {
+			closeLoading();
+		}
+		// closeLoading();
+		if (data.code === 500001) {
 			//not authorized
-			message.error(data.msg);
-			storage.remove("token");
-			location.href = "/#/login";
+			message.error("登录信息已过期，请重新登录");
+			setTimeout(() => {
+				storage.remove("token");
+				location.href = "/login?callback=" + encodeURIComponent;
+			}, 1000);
 		} else if (data.code !== 0) {
 			//api error, 0 means success, other code means error
-			message.error(data.msg);
-			// location.href = "/#/login";
-			return Promise.reject(data.msg);
+			if (response.config.showError === false) {
+				return Promise.resolve(data);
+			} else {
+				message.error(data.msg);
+				return Promise.reject(data.msg);
+			}
 		}
 		return data.data;
 	},
@@ -54,12 +66,15 @@ instance.interceptors.response.use(
 		return Promise.reject(error.message);
 	}
 );
-
+interface IConfig {
+	showLoading?: boolean;
+	showError?: boolean;
+}
 export default {
-	get<T>(url: string, params?: object): Promise<T> {
-		return instance.get(url, { params });
+	get<T>(url: string, params?: object, options: IConfig = { showLoading: true, showError: true }): Promise<T> {
+		return instance.get(url, { params, ...options });
 	},
-	post<T>(url: string, data?: object): Promise<T> {
-		return instance.post(url, data);
+	post<T>(url: string, data?: object, options: IConfig = { showLoading: true, showError: true }): Promise<T> {
+		return instance.post(url, data, options);
 	},
 };
