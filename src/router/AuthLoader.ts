4838fd358a36dd7d5ddfffddea4c3d0da504ab55
Liang <lianginchina1999@gmail.com>
2024-07-21 16:02:35 +0800

    auth info loader before accessing layout page

---
+++ src/router/AuthLoader.ts
@@ -0,0 +1,20 @@
+import api from "@/api";
+import { Menu } from "@/types/api";
+import { getMenuPath } from "@/utils";
+
+export interface IAuthLoader {
+	buttonList: string[];
+	menuList: Menu.MenuItem[];
+	menuPathList: string[];
+}
+
+const AuthLoader = async () => {
+	const data = await api.getPermissionList();
+	const menuPathList = getMenuPath(data.menuList);
+	return {
+		buttonList: data.buttonList,
+		menuList: data.menuList,
+		menuPathList: menuPathList,
+	};
+};
+export default AuthLoader;
