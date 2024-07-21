5bfc7bfb3b28ba7bdc9946fee8d1158ed4d2de74
Liang <lianginchina1999@gmail.com>
2024-07-21 16:21:41 +0800

    add logout feature

--- src/components/NavHeader/index.tsx
+++ src/components/NavHeader/index.tsx
@@ -3,15 +3,19 @@ import { Breadcrumb, Dropdown, Switch } from "antd";
 import type { MenuProps } from "antd";
 import styles from "./index.module.less";
 import { useBearStore } from "@/store";
+import storage from "@/utils/storage";
 const NavHeader = () => {
 	const store = useBearStore();
 	const breadcrumbList = [{ title: "首页" }, { title: "工作台" }];
+	const handleLogout = () => {
+		storage.remove("token");
+		// location.href = "/login?callback=" + encodeURIComponent(location.href);
+	};
 	const items: MenuProps["items"] = [
 		{ key: "1", label: "个人中心" },
 		{ key: "2", label: "邮箱:" + store.userInfo.userEmail, disabled: true },
-		{ key: "3", label: "退出" + store.token },
+		{ key: "3", label: <a onClick={handleLogout}>退出</a> },
 	];
-
 	return (
 		<div className={styles.navHeader}>
 			<div className={styles.left}>
